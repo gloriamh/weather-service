24704def4627ffa2b3ff7e2cc1d7830e420a4613
Gloria Martinez <gloria.martinez@careesma.com>
2019-11-24 20:05:38 +0100

    Use reqwest::Response.json()

--- src/weather.rs
+++ src/weather.rs
@@ -2,8 +2,7 @@ pub fn get_current_temperature(city: String, units: String) -> String {
     let url_str = format!(
         "http://api.openweathermap.org/data/2.5/weather?q={}&units={}&APPID=347492b4193159e8965692864ec168ea",
         city, units);
-    let body = reqwest::get(&url_str).unwrap().text().unwrap();
-    let json_response : serde_json::Value = serde_json::from_str(&body).unwrap();
+    let json_response: serde_json::Value = reqwest::get(&url_str).unwrap().json().unwrap();
     json_response["main"]["temp"].to_string()
 }
 
@@ -11,7 +10,6 @@ pub fn get_forecast(city: String, units: String) -> String {
     let url_str = format!(
         "http://api.openweathermap.org/data/2.5/forecast?q={}&units={}&APPID=347492b4193159e8965692864ec168ea",
         city, units);
-    let body = reqwest::get(&url_str).unwrap().text().unwrap();
-    let json_response : serde_json::Value = serde_json::from_str(&body).unwrap();
+    let json_response: serde_json::Value = reqwest::get(&url_str).unwrap().json().unwrap();
     serde_json::json!(json_response["list"]).to_string()
 }
