86246dd7ede188752bfad35455235961d7bc0d8e
Gloria Martinez <gloria.martinez@careesma.com>
2019-11-24 22:01:38 +0100

    WIP: Added tests

--- src/weather.rs
+++ src/weather.rs
@@ -1,8 +1,12 @@
+fn req_get_json(url_str: &str) -> serde_json::Value {
+    reqwest::get(url_str).unwrap().json().unwrap()
+}
+
 pub fn get_current_temperature(city: String, units: String) -> String {
     let url_str = format!(
         "http://api.openweathermap.org/data/2.5/weather?q={}&units={}&APPID=347492b4193159e8965692864ec168ea",
         city, units);
-    let json_response: serde_json::Value = reqwest::get(&url_str).unwrap().json().unwrap();
+    let json_response: serde_json::Value = req_get_json(&url_str);
     json_response["main"]["temp"].to_string()
 }
 
@@ -10,6 +14,20 @@ pub fn get_forecast(city: String, units: String) -> String {
     let url_str = format!(
         "http://api.openweathermap.org/data/2.5/forecast?q={}&units={}&APPID=347492b4193159e8965692864ec168ea",
         city, units);
-    let json_response: serde_json::Value = reqwest::get(&url_str).unwrap().json().unwrap();
+    let json_response: serde_json::Value = req_get_json(&url_str);
     serde_json::json!(json_response["list"]).to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_current_temperature() {
+        let city = String::from("London");
+        let units = String::from("metric");
+        let current_temperature = get_current_temperature(city, units);
+        // TODO: Mock openweathermap response
+        // assert_eq!(current_temperature, "10");
+    }
 }
